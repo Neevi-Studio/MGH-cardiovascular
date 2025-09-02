f7218cebe3d45d6ca5365b6e3e4e84e08a6de56b
karoladelk <karoladelk@gmail.com>
2025-09-02 17:19:25 +0300

    optimizing seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -182,7 +182,7 @@ export default function RootLayout({
       <link rel="preconnect" href="https://maps.googleapis.com" />
       <link rel="preconnect" href="https://maps.gstatic.com" crossOrigin="anonymous" />
     {/* HTML Meta Tags */}
-    <title>MGH Cardiovascular Associates | Premier Heart Care</title>
+    {/* <title>MGH Cardiovascular Associates | Premier Heart Care</title> */}
     <meta name="description" content="At MGH Cardiovascular Associates, we offer the highest level of heart care. Our expert cardiologists provide personalized treatments, cutting-edge diagnostics, and comprehensive services for heart disease prevention and management." />
 
     {/* Facebook Meta Tags */}
